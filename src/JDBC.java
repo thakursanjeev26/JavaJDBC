c1c06d6e2261a7866c6b5a6d1c52f5b5ff0462f6
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-09-16 14:16:57 +0530

    Added:UC1_Created_Connection_Successfully

--- src/JDBC.java
+++ src/JDBC.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class JDBC {
     public static void main(String[] args) {
-        System.out.println("Welcome To JDBC");
+        System.out.println("Welcome To JDBC ");
 
         String url="jdbc:mysql://127.0.0.1:3306/dbdemo";
         String user="root";
