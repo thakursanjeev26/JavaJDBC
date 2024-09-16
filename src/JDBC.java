c8e5c9fc634c1bf4801b78e41b227e4a6d0b996b
thakursanjeev26 <sanjeevjthakur@gmail.com>
2024-09-16 15:09:58 +0530

    Added:Updated_Data_sucessfully

--- src/JDBC.java
+++ src/JDBC.java
@@ -29,15 +29,17 @@ public class JDBC {
 //                System.out.println("Date: "+start_date);
 //            }
             Statement statement = connection.createStatement();
-            String query = String.format("INSERT INTO employee_payroll (name, salary, start_date) VALUES ('%s', %f, '%s')","Sanjeev",300000.00,"2020-06-26");
+//            String query = String.format("INSERT INTO employee_payroll (name, salary, start_date) VALUES ('%s', %f, '%s')","Sanjeev",300000.00,"2020-06-26");
 //
+            String query = String.format("UPDATE employee_payroll SET name='%s' WHERE id=%d", "Abhi", 2);
+
             int rowsAffected = statement.executeUpdate(query);
 
             if(rowsAffected>0){
-                System.out.println("Data Inserted Successfully");
+                System.out.println("Data Updated Successfully");
             }
             else {
-                System.out.println("Data not Inserted");
+                System.out.println("Data not Updated");
             }
         }
         catch (SQLException e){
